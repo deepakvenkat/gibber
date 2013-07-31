6a5d7598b99cd927c36f7e7831b650491939ad7f
deepakvenkat <dipak2911@gmail.com>
2013-07-30 20:07:26 -0400

    included jade and rendered the initial page

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express  = require("express");
 var app = express();
 var port = 3700;
-app.get("/", function (req, res) {
-  res.send("It works");
-})
+app.set('views', __dirname + '/templates');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
 
 app.listen(port);
 console.log("Listening on port" + port)
